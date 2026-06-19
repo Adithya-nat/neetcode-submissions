444bd59af7597f503cd47f53a6aed79f66069353
Adithya-nat <52786980+Adithya-nat@users.noreply.github.com>
2026-06-18 19:39:29 -0700

    Add: palindrome-number - submission-0

---
+++ Data Structures & Algorithms/palindrome-number/submission-0.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x < 0 or ( x % 10 == 0 and x != 0):
+            return False
+        
+        rh = 0
+
+        while x > rh:
+            digit = x % 10
+            rh = ( rh * 10 ) + digit
+            x = x / 10
+        
+        return x == rh or x == (rh/10)
+        
